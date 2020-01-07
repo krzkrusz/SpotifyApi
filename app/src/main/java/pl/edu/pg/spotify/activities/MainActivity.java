a3b1195f1772d5faed6724478ad993d3632e263e
kkrus <kkruszynski1993@wp.pl>
2020-01-07 23:39:13 +0100

    spotify api connected

--- app/src/main/java/pl/edu/pg/spotify/activities/MainActivity.java
+++ app/src/main/java/pl/edu/pg/spotify/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import pl.edu.pg.spotify.R;
+import pl.edu.pg.spotify.util.SpotifyUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SpotifyUtils.connect(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyUtils.disconnect();
+    }
+
 
 }
--- app/src/main/java/pl/edu/pg/spotify/util/SpotifyUtils.java
+++ app/src/main/java/pl/edu/pg/spotify/util/SpotifyUtils.java
@@ -1,6 +1,65 @@
 package pl.edu.pg.spotify.util;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.mappers.jackson.JacksonMapper;
+import com.spotify.protocol.types.Track;
+
+
 public class SpotifyUtils {
+    private static final String CLIENT_ID = "437d0cfe01824ddaaadc89bf865052b9";
+    private static final String REDIRECT_URI = "http://localhost:8888/callback";
+    private static SpotifyAppRemote mSpotifyAppRemote;
 
+    public static void connect(Context context) {
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .setJsonMapper(JacksonMapper.create())
+                        .showAuthView(true)
+                        .build();
 
+        SpotifyAppRemote.connect(context, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+
+                    }
+
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MyActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    private static void connected() {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                });
+    }
+
+    public static void disconnect() {
+        if (mSpotifyAppRemote != null)
+            SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
 }
