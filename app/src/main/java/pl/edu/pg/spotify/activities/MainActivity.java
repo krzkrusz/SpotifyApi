6be4f20b4cb5d8acdea7c1cda1ad898844735496
axel <formella.alex@gmail.com>
2020-02-06 20:39:24 +0100

    The device info moved into DeviceUtil class.

--- app/src/main/java/pl/edu/pg/spotify/activities/MainActivity.java
+++ app/src/main/java/pl/edu/pg/spotify/activities/MainActivity.java
@@ -1,9 +1,5 @@
 package pl.edu.pg.spotify.activities;
 
-import android.app.ActivityManager;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -11,10 +7,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import pl.edu.pg.spotify.R;
+import pl.edu.pg.spotify.util.DeviceUtil;
 import pl.edu.pg.spotify.util.SpotifyUtils;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,17 +26,15 @@ public class MainActivity extends AppCompatActivity {
         cpuTextView = findViewById(R.id.textView);
         ramTextView = findViewById(R.id.textView2);
 
-        cpuTextView.setText(getCurrentCPUusage());
-        ramTextView.setText(getCurrentRAMUsage());
-
+        cpuTextView.setText(DeviceUtil.getCurrentCPUusage());
+        ramTextView.setText(DeviceUtil.getCurrentRAMUsage(getSystemService(ACTIVITY_SERVICE)));
+        DeviceUtil.getNetworkInfo(getApplicationContext());
         updateUI();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        //SpotifyUtils.connect(this);
     }
 
     @Override
@@ -53,58 +45,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateUI(){
         mRedrawHandler.sleep(1000);
-        cpuTextView.setText(getCurrentCPUusage());
-        ramTextView.setText(getCurrentRAMUsage());
-        getNetworkInfo();
+        cpuTextView.setText(DeviceUtil.getCurrentCPUusage());
+        ramTextView.setText(DeviceUtil.getCurrentRAMUsage(getSystemService(ACTIVITY_SERVICE)));
+        DeviceUtil.getNetworkInfo(getApplicationContext());
     }
-
-    private String getCurrentCPUusage() {
-        ProcessBuilder processBuilder;
-        String holder = "";
-        String[] command = {"sh","-c", "top -n 1"};
-        InputStream inputStream;
-        Process process;
-        byte[] byteArry;
-        byteArry = new byte[1024];
-        try{
-            processBuilder = new ProcessBuilder(command);
-            process = processBuilder.start();
-            inputStream = process.getInputStream();
-            while(inputStream.read(byteArry) != -1){
-                holder = holder + new String(byteArry);
-            }
-            inputStream.close();
-        } catch(IOException ex){
-            ex.printStackTrace();
-        }
-        final String clearedFromNewLines = holder.replace("\n", "");
-        final int onlyCPUusageLastIndex = clearedFromNewLines.indexOf("IOW");
-        return clearedFromNewLines.substring(0, onlyCPUusageLastIndex - 1);
-    }
-
-
-    private String getCurrentRAMUsage() {
-        final long bytesInMegabyte = 1048576L;
-        final ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
-        final ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-        activityManager.getMemoryInfo(mi);
-        return (mi.totalMem - mi.availMem) / bytesInMegabyte + "MB";
-    }
-
-    private void getNetworkInfo() {
-        final ConnectivityManager cm = (ConnectivityManager) getApplicationContext()
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-        final NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        if (activeNetwork != null) {
-            // np. MOBILE lub WIFI
-            final String networkType = activeNetwork.getTypeName();
-            // np. LTE
-            final String subTypeName = activeNetwork.getSubtypeName();
-        } else {
-            // not connected to the internet
-        }
-    }
-
 
     class RefreshHandler extends Handler {
         @Override
--- app/src/main/java/pl/edu/pg/spotify/util/DeviceUtil.java
+++ app/src/main/java/pl/edu/pg/spotify/util/DeviceUtil.java
@@ -1,4 +1,60 @@
 package pl.edu.pg.spotify.util;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class DeviceUtil {
+
+    public static String getCurrentCPUusage() {
+        ProcessBuilder processBuilder;
+        String holder = "";
+        String[] command = {"sh","-c", "top -n 1"};
+        InputStream inputStream;
+        Process process;
+        byte[] byteArry;
+        byteArry = new byte[1024];
+        try{
+            processBuilder = new ProcessBuilder(command);
+            process = processBuilder.start();
+            inputStream = process.getInputStream();
+            while(inputStream.read(byteArry) != -1){
+                holder = holder + new String(byteArry);
+            }
+            inputStream.close();
+        } catch(IOException ex){
+            ex.printStackTrace();
+        }
+        final String clearedFromNewLines = holder.replace("\n", "");
+        final int onlyCPUusageLastIndex = clearedFromNewLines.indexOf("IOW");
+        return clearedFromNewLines.substring(0, onlyCPUusageLastIndex - 1);
+    }
+
+
+    public static String getCurrentRAMUsage(final Object systemService) {
+        final long bytesInMegabyte = 1048576L;
+        final ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+        final ActivityManager activityManager = (ActivityManager) systemService;
+        activityManager.getMemoryInfo(mi);
+        return (mi.totalMem - mi.availMem) / bytesInMegabyte + "MB";
+    }
+
+    public static void getNetworkInfo(final Context ctx) {
+        final ConnectivityManager cm = (ConnectivityManager)
+                ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (activeNetwork != null) {
+            // np. MOBILE lub WIFI
+            final String networkType = activeNetwork.getTypeName();
+            // np. LTE
+            final String subTypeName = activeNetwork.getSubtypeName();
+        } else {
+            // not connected to the internet
+        }
+    }
+
 }
